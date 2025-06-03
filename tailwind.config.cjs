b7d3c51a03f6940acd43a682a820075461a25bb6
LemonFan-maker <2254650260@qq.com>
2025-06-03 17:18:56 +0800

    更新字体修改

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-}
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+	darkMode: "class", // allows toggling dark mode manually
+	theme: {
+		extend: {
+			fontFamily: {
+				// sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+				// 字体设置可以参考这里进行抄写
+				sans: [
+					"LXGWWenKai",
+					"Roboto",
+					"sans-serif",
+					...defaultTheme.fontFamily.sans,
+				],
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
+};
